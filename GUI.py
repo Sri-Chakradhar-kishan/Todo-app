0a72da3bf0af434b78b1c6416bf5fff416d65e9d
srichakradhar <sri.chakradhar12@gmail.com>
2023-02-17 22:00:05 +0530

    Implement the complete and exit buttons

--- GUI.py
+++ GUI.py
@@ -7,8 +7,14 @@ add_button = PySimpleGUI.Button("Add")
 list_box = PySimpleGUI.Listbox(values=functions.get_todos(), key='todos',
                                enable_events=True, size=[45, 10])
 edit_button = PySimpleGUI.Button("Edit")
+complete_button = PySimpleGUI.Button("Complete")
+exit_button = PySimpleGUI.Button("Exit")
+
 window = PySimpleGUI.Window('My To-Do App',
-                            layout=[[label, input_box, add_button], [list_box, edit_button]],
+                            layout=[[label],
+                                    [input_box, add_button],
+                                    [list_box, edit_button, complete_button],
+                                    [exit_button]],
                             font=('Helvetica', 10))
 while True:
     event, values = window.read()
@@ -32,6 +38,15 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case "Exit":
+            break
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case PySimpleGUI.WIN_CLOSED:
